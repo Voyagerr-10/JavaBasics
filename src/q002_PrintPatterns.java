c56514efaf595ba8b629926e24ab4aa1b604cb52
Voyagerr-10 <karanthakur150967@gmail.com>
2025-10-23 10:14:08 +0530

    PrintPatterns

---
+++ src/q002_PrintPatterns.java
@@ -0,0 +1,27 @@
+/*q002: Print these patterns:
+        1. Right Half pyramid
+        2. Reverse Right Half Pyramid
+        3. Left Half Pyramid*/
+
+public class q002_PrintPatterns {
+    public static void main(String[] args) {
+        System.out.println("Pattern 1: Right Half Pyramid");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        System.out.println("Pattern 2: Reverse Right Half Pyramid");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+        System.out.println("Pattern 3: Left Half Pyramid");
+        System.out.println("* * * * *");
+        System.out.println("  * * * *");
+        System.out.println("    * * *");
+        System.out.println("      * *");
+        System.out.println("        *");
+    }
+}
